f51ce8e815f6c449f8ea6038b42a897c47f15b36
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-01-12 19:53:37 +0300

    simple update

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-let newX = 0,newY=0 ,startX=0 ,startY=0
+let startX= 0, startY=0, newX=0, newY=0;
 
-const draggables = document.getElementById('.draggable')
+let draggableElement = document.getElementById('draggable')
 
 // let containers = document.querySelector('background')
 
+draggableElement.addEventListener('mousedown', mouseDown)
 
-draggables.addEventListener('mousedown', mouseDown)
+
+
 
 function mouseDown(e){
-   startX=e.clientX
-   startY=e.clientY
-
-
-   document.addEventListener('mousemove', mouseMove)
-   document.addEventListener('mouseup', mouseUp)
-}
-
-function mouseMove(e){
-    newX = startX - e.clientX
-    newY = startY - e.clientY
-
-
     startX = e.clientX
     startY = e.clientY
 
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
 
-    draggables.style.top = (draggables.style.top - newY) + 'px'
-    draggables.style.left =(draggables.style.left - newX) + 'px'
-
-
-    console.log(newY)
 }
 
 
-function mouseUp (e){
+function mouseMove(e){
+   newX = startX - e.clientX
+   newY = startY - e.clientY
+
+
+   startX = e.clientX
+   startY = e.clientY
+
+   draggableElement.style.top = startY + 'px'
+   draggableElement.style.left = startX + 'px'
+}
+
+function mouseUp(e){
     document.removeEventListener('mousemove', mouseMove)
 }
-
